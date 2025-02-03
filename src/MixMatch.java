1b9792a5ff65d128abe65b3e4cdd5966dceef6f7
Ismael Velazquez <128411935+Ivela183@users.noreply.github.com>
2025-02-03 00:57:57 +0000

    First Commit

---
+++ src/MixMatch.java
@@ -0,0 +1,20 @@
+public class MixMatch{
+    public static void main(String[] arg){
+        int varInt = 8;
+        System.out.println(varInt + " <- Integer, stores whole numbers from -2,147,483,648 and 2,147,483,648");
+        String varString = "Variable String";
+        System.out.println(varString + " <- String, stores any words within quotation marks");
+        char varChar = '&';
+        System.out.println(varChar + " <- Character, stores any character such as symbols, letters, and numbers");
+        short varShort = 30902;
+        System.out.println(varShort + " <- Short, stores whole numbers from -32,768 and 32,768");
+        long varLong = 304580309L;
+        System.out.println(varLong + " <- Long, stores whole numbers from -9,223,372,036,854,775,808 and 9,223,372,036,854,775,808");
+        float varFloat = 2.4F;
+        System.out.println(varFloat + " <- Float, stores fractional numbers up to 7 decimal digits");
+        double varDouble = 8.6456546;
+        System.out.println(varDouble + " <- Double, stores fractional numbers up to 15 decimal digits");
+        boolean varBoolean = true;
+        System.out.println(varBoolean + " <- Boolean, stores true or false value");
+    }
+}
